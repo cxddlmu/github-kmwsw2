304d461f6013d19c5ec8545f829b2276a473e8f6
崔晓东 <xiaodong.cui.sh@dianping.com>
2019-12-10 21:29:29 +0800

    make home environment work and add antV

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,17 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { MainModule } from './main/main.module'
 
 const routes: Routes = [
   {
     path: 'main',
-    loadChildren: 'app/main/main.module#MainModule'
+    loadChildren: () => import('./main/main.module').then(m => m.MainModule)
   },
   {
     path: '',
-    redirectTo: '/main',pathMatch:'full'
+    redirectTo: '/main', pathMatch: 'full'
   },
-  
+
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,6 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import {MainModule} from './main/main.module'
 import { DragDropModule } from '@angular/cdk/drag-drop';
 import { ScrollingModule } from '@angular/cdk/scrolling';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
