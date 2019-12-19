54c33991c71fb87a5f1a6fbb25e4175db575ee3d
cxddlmu <cxd_dlmu@126.com>
2019-12-19 17:51:01 +0800

    1 file modified, 2 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,12 +8,14 @@ import { ScrollingModule } from '@angular/cdk/scrolling';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientJsonpModule, HttpClientModule } from '@angular/common/http';
-import { SharedModule } from './shared/shared.module'
+import { SharedModule } from './shared/shared.module';
+import { FormRuleEngineComponent } from './form-rule-engine/form-rule-engine.component'
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormRuleEngineComponent
   ],
   imports: [
     BrowserModule, FormsModule, HttpClientModule, HttpClientJsonpModule,
---
+++ src/app/form-rule-engine/form-rule-engine.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-form-rule-engine',
+  templateUrl: './form-rule-engine.component.html',
+  styleUrls: ['./form-rule-engine.component.css']
+})
+export class FormRuleEngineComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
