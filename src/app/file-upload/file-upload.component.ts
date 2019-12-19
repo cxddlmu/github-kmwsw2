c7ad019de977e1ee27f3ca0fc36d60f61609cdd7
cxddlmu <cxd_dlmu@126.com>
2019-12-19 15:03:13 +0800

    2 files modified

--- src/app/file-upload/file-upload.component.ts
+++ src/app/file-upload/file-upload.component.ts
@@ -5,7 +5,11 @@ import FilePondPluginFileRename from 'filepond-plugin-file-rename';
 
 // Register the plugin
 FilePond.registerPlugin(FilePondPluginFileRename);
+// Import the plugin code
+import FilePondPluginFileEncode from 'filepond-plugin-file-encode';
 
+// Register the plugin
+FilePond.registerPlugin(FilePondPluginFileEncode);
 @Component({
   selector: 'app-file-upload',
   templateUrl: './file-upload.component.html',
@@ -35,16 +39,36 @@ export class FileUploadComponent implements OnInit {
   pondHandleInit() {
     console.log('FilePond has initialised', this.myPond);
   }
-
+/**
+ * id	Returns the id of the file
+serverId	Returns the server id of the file
+origin	Returns the origin of the file, either input ( added by user ), limbo ( temporary server file ) or local ( existing server file )
+status	Returns the current status of the file, use the FilePond.FileStatus enum to determine the status.
+file	Returns the File object
+fileExtension	Returns the file extension
+fileSize	Returns the size of the file
+filename	Returns the full name of the file
+filenameWithoutExtension	Returns the name of the file without extension
+ */
   pondHandleAddFile(event: any) {
     type fileData={
       [filename:string]:string
     }
-    let file:fileData={};
+    let file:fileData={
+      filename:event.pond.getFile().filename,
+      id:event.pond.getFile().id,
+      fileSize:event.pond.getFile().fileSize,
+      fileExtension:event.pond.getFile().fileExtension,
+      filenameWithoutExtension:event.pond.getFile().filenameWithoutExtension,
+      serverId:event.pond.getFile().serverId,
+      origin:event.pond.getFile().origin,
+      status:event.pond.getFile().status,
+    getFileEncodeDataURL:event.pond.getFile().getFileEncodeDataURL(),
+    getFileEncodeBase64String:event.pond.getFile().getFileEncodeBase64String()
+    };
 
-    file=event.pond.getFile().filename
-    console.log('A file was added', event.pond.getFile());
-    console.log('A file was added', event.pond.getFile().filename);
+    console.log('A file was added', event.pond.getFile().getFileEncodeDataURL());
+    console.log('A file was added', event.pond.getFile().getFileEncodeBase64String());
     console.log('A file was added', file);
   }
 
