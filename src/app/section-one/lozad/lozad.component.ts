27e02c5e161fcac909c53170e3d79933ba831e9b
cxddlmu <cxd_dlmu@126.com>
2020-01-02 16:23:35 +0800

    1 file modified, 1 added

--- src/app/section-one/lozad/lozad.component.ts
+++ src/app/section-one/lozad/lozad.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit } from "@angular/core";
+import lozad from "lozad";
 @Component({
-  selector: 'app-lozad',
-  templateUrl: './lozad.component.html',
-  styleUrls: ['./lozad.component.css']
+  selector: "app-lozad",
+  templateUrl: "./lozad.component.html",
+  styleUrls: ["./lozad.component.css"]
 })
 export class LozadComponent implements OnInit {
-
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
-  }
+    
+    const el = document.querySelector("img");
+    const observer = lozad(el, {
+      load: function(el) {
+        console.log("loading element");
 
+        // Custom implementation to load an element
+        // e.g. el.src = el.getAttribute('data-src');
+      }
+    });
+    observer.observe();
+  }
 }
