abb19867869ee45b1e86278455f38c4ccf273da3
cxddlmu <cxd_dlmu@126.com>
2020-01-02 17:52:14 +0800

    2 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,13 +9,13 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientJsonpModule, HttpClientModule } from '@angular/common/http';
 import { SharedModule } from './shared/shared.module';
-import { FormRuleEngineComponent } from './form-rule-engine/form-rule-engine.component'
+// import { FormRuleEngineComponent } from './form-rule-engine/form-rule-engine.component'
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    FormRuleEngineComponent
+    // FormRuleEngineComponent
   ],
   imports: [
     BrowserModule, FormsModule, HttpClientModule, HttpClientJsonpModule,
