960238e96401f85eff65f9e3ecfd50721e4cd4c6
cxddlmu <cxd_dlmu@126.com>
2020-01-03 11:08:55 +0800

    1 file modified

--- src/app/section-one/photoviewer/photoviewer.component.ts
+++ src/app/section-one/photoviewer/photoviewer.component.ts
@@ -13,9 +13,17 @@ export class PhotoviewerComponent implements OnInit {
     console.log($event.target);
     var items = [
       {
-        src: "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1577963382225&di=4cd3e6f96659512e213c7a40370ce1cd&imgtype=0&src=http%3A%2F%2Fwww.people.com.cn%2Fmediafile%2Fpic%2F20200102%2F66%2F307688541549362682.jpg", // path to image
+        src: "https://raw.githubusercontent.com/ApoorvSaxena/lozad.js/master/demo/images/thumbs/02.jpg", // path to image
         title: "Image Caption 1" // If you skip it, there will display the original image name(image1)
-      }
+      },
+            {
+        src: "https://raw.githubusercontent.com/ApoorvSaxena/lozad.js/master/demo/images/thumbs/03.jpg", // path to image
+        title: "Image Caption 1" // If you skip it, there will display the original image name(image1)
+      },
+            {
+        src: "https://raw.githubusercontent.com/ApoorvSaxena/lozad.js/master/demo/images/thumbs/04.jpg", // path to image
+        title: "Image Caption 1" // If you skip it, there will display the original image name(image1)
+      },
     ];
 
     // define options (if needed)
