07d9ec9162748392967e1d9476e73fdb00800aaf
cxddlmu <cxd_dlmu@126.com>
2020-01-06 17:17:51 +0800

    2 files modified

--- src/app/section-one/editor-js/editor-js.component.ts
+++ src/app/section-one/editor-js/editor-js.component.ts
@@ -1,33 +1,38 @@
-import { Component, OnInit } from '@angular/core';
-import EditorJS from '@editorjs/editorjs';
-import Header from '@editorjs/header'; 
-import List from '@editorjs/list'; 
+import { Component, OnInit } from "@angular/core";
+import EditorJS from "@editorjs/editorjs";
+import Header from "@editorjs/header";
+import List from "@editorjs/list";
 @Component({
-  selector: 'app-editor-js',
-  templateUrl: './editor-js.component.html',
-  styleUrls: ['./editor-js.component.css']
+  selector: "app-editor-js",
+  templateUrl: "./editor-js.component.html",
+  styleUrls: ["./editor-js.component.css"]
 })
 export class EditorJSComponent implements OnInit {
-
-  constructor() { }
-
+  constructor() {}
+  editor;
   ngOnInit() {
-    const editor = new EditorJS({ 
-  /** 
-   * Id of Element that should contain the Editor 
-   */ 
-  holderId: 'editorjs', 
-  
-  /** 
-   * Available Tools list. 
-   * Pass Tool's class or Settings object for each Tool you want to use 
-   */ 
-  tools: { 
-    header: Header, 
-    list: List 
-  }, 
-})
+    this.editor = new EditorJS({
+      /**
+       * Id of Element that should contain the Editor
+       */
 
+      holderId: "editorjs",
+
+      /**
+       * Available Tools list.
+       * Pass Tool's class or Settings object for each Tool you want to use
+       */
+
+      tools: {
+        header: Header,
+        list: List
+      }
+    });
   }
 
+  save() {
+    this.editor.save().then(savedData => {
+      console.log(savedData);
+    });
+  }
 }
