9d9d7b44cbd8435745f5094142ecdf224922dd08
cxddlmu <cxd_dlmu@126.com>
2020-01-08 10:51:07 +0800

    1 file modified

--- src/app/section-one/cleavejs/cleavejs.component.ts
+++ src/app/section-one/cleavejs/cleavejs.component.ts
@@ -1,16 +1,20 @@
-import { Component, OnInit } from '@angular/core';
-import Cleave from 'cleave.js';
+import { Component, OnInit } from "@angular/core";
+import Cleave from "cleave.js";
 
 @Component({
-  selector: 'app-cleavejs',
-  templateUrl: './cleavejs.component.html',
-  styleUrls: ['./cleavejs.component.css']
+  selector: "app-cleavejs",
+  templateUrl: "./cleavejs.component.html",
+  styleUrls: ["./cleavejs.component.css"]
 })
 export class CleavejsComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
+  ngOnInit() {}
+  ngAfterViewInit() {
+    const cleave = new Cleave(".input-phone", {
+      date: true,
+      delimiter: "-",
+      datePattern: ["Y", "m", "d"]
+    });
   }
-
 }
