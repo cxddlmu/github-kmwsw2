16c8d065f4c36650837755a1a07bd797753871ce
cxddlmu <cxd_dlmu@126.com>
2020-01-09 16:22:14 +0800

    3 files modified

--- src/app/section-one/handy-scroll/handy-scroll.component.ts
+++ src/app/section-one/handy-scroll/handy-scroll.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from "@angular/core";
-import * as handyScroll from 'handy-scroll'
+import * as handyScroll from "handy-scroll";
 @Component({
   selector: "app-handy-scroll",
   templateUrl: "./handy-scroll.component.html",
@@ -9,36 +9,30 @@ export class HandyScrollComponent implements OnInit {
   constructor() {}
 
   ngOnInit() {
+    
+
+  }
+  ngAfterViewInit(){
     let handscrollsContainer = document.getElementById("handscrolls");
     let handscrolls = handscrollsContainer.getElementsByTagName("img");
     handscrollsContainer.scrollLeft = handscrollsContainer.scrollWidth;
-
     // Widget initialization
     handyScroll.mount(handscrollsContainer);
 
-    document
-      .getElementById("handscrolls-nav")
-      .addEventListener("click", ({ target }) => {
-        if (target.matches("span:not(.active)")) {
-          Array.from(target.parentNode.children).forEach((span, index) => {
-            let isActive = span === target;
-            span.classList.toggle("active", isActive);
-            handscrolls[index].classList.toggle("active", isActive);
-          });
-          handscrollsContainer.scrollLeft = handscrollsContainer.scrollWidth;
-          // Force widget update programmatically
-          handyScroll.update(handscrollsContainer);
-        }
-      });
-
-    document
-      .getElementById("handscrolls-toggle")
-      .addEventListener("click", ({ target }) => {
-        // Checking whether the widget is mounted or not
-        let mounted = handyScroll.mounted(handscrollsContainer);
-        target.classList.toggle("disabled", mounted);
-        // Unmount/remount the widget
-        handyScroll[mounted ? "destroy" : "mount"](handscrollsContainer);
-      });
+    // document
+    //   .getElementById("handscrolls-nav")
+    //   .addEventListener("click", ({ target }) => {
+    //     if (target.matches("span:not(.active)")) {
+    //       Array.from(target.parentNode.children).forEach((span, index) => {
+    //         let isActive = span === target;
+    //         span.classList.toggle("active", isActive);
+    //         handscrolls[index].classList.toggle("active", isActive);
+    //       });
+    //       handscrollsContainer.scrollLeft = handscrollsContainer.scrollWidth;
+    //       // Force widget update programmatically
+    //       handyScroll.update(handscrollsContainer);
+    //     }
+    //   });
   }
+  
 }
